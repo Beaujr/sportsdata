69b79ed32520956b97047044550bf0bced67418e
Beau Rudder <beau@rosterbot.com>
2014-07-24 21:51:32 -0700

    API file to hook into Classes

---
+++ PHP/API.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: beaurudder
+ * Date: 2014-07-24
+ * Time: 8:17 PM
+ */
+include("SportDataAPI.php");
+$SportsData = new SportDataApi();
+
+if ($_GET["service"] == 'centre' && $_GET['id'] != null) {
+    $SportsData->loadCentreData($_GET['id']);
+    echo json_encode($SportsData->loadLeagues($_GET['id']));
+} else if ($_GET['service'] == 'league' && $_GET['id'] != null) {
+    $SportsData->loadLeagueData($_GET['id']);
+    echo json_encode($SportsData->loadDivisions());
+} else if ($_GET['service'] == 'division' && $_GET['id'] != null) {
+    $SportsData->loadDivisionData($_GET['id']);
+    echo json_encode($SportsData->loadTeams());
+} else if ($_GET['service'] == 'score' && $_GET['id'] != null) {
+    $SportsData->loadDivisionData($_GET['id']);
+    $scoreArray = array('Score' => $SportsData->pastScores_By_Game(), 'Teams' => $SportsData->loadTeams());
+    echo json_encode($scoreArray);
+}
+?>
